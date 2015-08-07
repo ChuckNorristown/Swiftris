f63af602100ddbb6d4dd7511660e06a7458a4699
Sean Shields <sean@dmgctrl.com>
2015-08-06 22:56:26 -0400

    adding random block color function

--- Swiftris/Swiftris/Block.swift
+++ Swiftris/Swiftris/Block.swift
@@ -11,6 +11,7 @@ import SpriteKit
 let NumberOf Columns: UInt32 = 6
 
 enum BlockColor: Int, Printable {
+    
     case Blue = 0, Orange, Purple, Red, Teal, Yellow
     
     var spriteName: String {
@@ -35,6 +36,37 @@ enum BlockColor: Int, Printable {
     }
     
     static func random() -> BlockColor {
-        return BlockColor(rawValue: <Int(arc4random_uniform(NumberOfColors)))!
+        return BlockColor(rawValue:Int(arc4random_uniform(NumberOfColors)))!
     }
+}
+
+class Block: Hashable, Printable {
+
+    let color: BlockColor
+
+    var column: Int
+    var row: Int
+    var sprite: SKSpriteNode?
+    
+    var spriteName: String {
+        return color.spriteName
+    }
+    
+    var hashValue: Int {
+        return self.column ^ self.row
+    }
+    
+    var description: String {
+        return"\(color): [\(column), \(row)]"
+    }
+    
+    init(column:Int, row:Int, color:BlockColor) {
+        self.column = column
+        self.row = row
+        self.color = color
+    }
+}
+
+func ==(lhs: Block, rhs: Block) -> Bool {
+    return lhs.column == rhs.column && lhs.row == rhs.row && lhs.color.rawValue == rhs.color.rawValue
 }
