0dc2808b801fb051ff03fff4532d65974c0fce2d
Sean Shields <sean@dmgctrl.com>
2015-08-07 13:15:22 -0400

    added Z shape

---
+++ Swiftris/Swiftris/ZShape.swift
@@ -0,0 +1,28 @@
+//
+//  ZShape.swift
+//  Swiftris
+//
+//  Created by Sean Shields on 8/7/15.
+//  Copyright (c) 2015 Sean Shields. All rights reserved.
+//
+
+class ZShape:Shape {
+    
+    override var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, roeDiff: Int)>] {
+        return [
+            Orientation.Zero:       [(1, 0), (1, 1), (0, 1), (0, 2)],
+            Orientation.Ninety:     [(-1,0), (0, 0), (0, 1), (1, 1)],
+            Orientation.OneEighty:  [(1, 0), (1, 1), (0, 1), (0, 2)],
+            Orientation.TwoSeventy: [(-1,0), (0, 0), (0, 1), (1, 1)]
+        ]
+    }
+    
+    override var bottomBlocksForOrientations: [Orientation: Array<Block>] {
+        return [
+            Orientation.Zero:       [blocks[SecondBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.Ninety:     [blocks[FirstBlockIdx], blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.OneEighty:  [blocks[SecondBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.TwoSeventy: [blocks[FirstBlockIdx], blocks[ThirdBlockIdx], blocks[FourthBlockIdx]]
+        ]
+    }
+}
