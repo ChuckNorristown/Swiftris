1f101b0a78581f8f752eb37eb6d54096682f5ff1
Sean Shields <sean@dmgctrl.com>
2015-08-07 11:03:46 -0400

    added square shape

---
+++ Swiftris/Swiftris/SquareShape.swift
@@ -0,0 +1,27 @@
+//
+//  SquareShape.swift
+//  Swiftris
+//
+//  Created by Sean Shields on 8/7/15.
+//  Copyright (c) 2015 Sean Shields. All rights reserved.
+//
+
+class SquareShape:Shape {
+    override var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, roeDiff: Int)>] {
+        return [
+            Orientation.Zero: [(0, 0), (1, 0), (0, 1), (1, 1)],
+            Orientation.OneEighty: [(0, 0), (1, 0), (0, 1), (1, 1)],
+            Orientation.Ninety: [(0, 0), (1, 0), (0, 1), (1, 1)],
+            Orientation.TwoSeventy: [(0, 0), (1, 0), (0, 1), (1, 1)]
+        ]
+    }
+    
+    override var bottomBlocksForOrientations: [Orientation: Array<Block>] {
+        return [
+            Orientation.Zero:       [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.OneEighty:  [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.Ninety:     [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.TwoSeventy: [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+        ]
+    }
+}
